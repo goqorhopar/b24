89fa41ed04e0ba753dd424ef275f6133e622fd15
goqorhopar <152752095+goqorhopar@users.noreply.github.com>
2025-09-10 19:24:15 +0300

    Update logger.js

--- src/logger.js
+++ src/logger.js
@@ -1,3 +1,4 @@
+// src/logger.js
 import pino from 'pino';
 
 // Проверяем, работает ли код на Render.com (в продакшене)
@@ -10,9 +11,14 @@ const logger = pino({
     target: 'pino-pretty',
     options: {
       translateTime: 'SYS:standard',
-      ignore: 'pid,hostname'
+      ignore: 'pid,hostname',
+      colorize: true
     }
-  }
+  },
+  formatters: {
+    level: (label) => ({ level: label })
+  },
+  timestamp: () => `,"time":"${new Date().toISOString()}"`
 });
 
 export default logger;
